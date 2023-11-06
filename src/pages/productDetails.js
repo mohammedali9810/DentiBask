d126c3aa25b4cb8be3ec2270e08d5034f2e8e71d
yahya mo <yahyamohmuedpro99@gmail.com>
2023-11-06 23:29:58 +0200

    update the product details page

--- src/pages/productDetails.js
+++ src/pages/productDetails.js
@@ -7,6 +7,7 @@ import { useDispatch } from 'react-redux';
 import { addToCart , decrementQuantity, incrementQuantity} from '../store/slices/cartslice';
 import { useSelector } from 'react-redux';
 import { Button } from 'react-bootstrap';
+import './style.css'
 
 export default function ProductDetails() {
   const [productDetails, setProductDetails] = useState([]);
@@ -31,7 +32,7 @@ export default function ProductDetails() {
   };
 
   return (
-    <Container>
+    <Container className="my-margin-top my-border p-3">
       <div className="row">
         <div className="col-lg-6  ">
           <img 
@@ -41,6 +42,7 @@ export default function ProductDetails() {
             }}
             src={selectedImage}
             alt={productDetails.title}
+            className="my-border"
           />
           <div className="d-flex flex-wrap justify-content-center">
             {productDetails.images?.map((image, index) => (
@@ -62,21 +64,23 @@ export default function ProductDetails() {
           </div>
         </div>
         <div className="col-lg-5 my-5 mx-2 ">
-          <h2>{productDetails.title}</h2>
-          <p>{productDetails.description}</p>
+          <h3>{productDetails.title}</h3>
+          <p className="m-3">{productDetails.description}</p>
+          <div className="m-3">
           <Rating rating={productDetails.rating} />
-          <span className="btn border-info border-2 rounded-pill m-3"><strong>Brand : </strong>{productDetails.brand}</span>
-          <span className="btn border-info border-2 rounded-pill "><strong>category : </strong>{productDetails.category}</span>
+          </div>
+          <span className="btn border-info border-1 rounded-pill m-3"><strong>Brand : </strong>{productDetails.brand}</span>
+          <span className="btn border-info border-1 rounded-pill "><strong>category : </strong>{productDetails.category}</span>
           <hr></hr>
-          <div className="row ">
-            <div  className="col-6  ">
+
+          <div className="row m-3 mt-5">
+            <div  className="">
               {productDetails.discountPercentage > 0 ? (
                 
                 <span>
-                   <span style={{fontSize:'20px' }} className="">Price Now : {productDetails.price - productDetails.price * productDetails.discountPercentage/100}</span>  
-
-                   <span className="m-2" style={{color:'red'}}> {productDetails.discountPercentage}%</span>
-                   <span className="m-2" style={{textDecoration:'line-through'}}>{productDetails.price}</span>
+                   <span style={{fontSize:'20px' }} className="">Price Now : <b>{(productDetails.price - productDetails.price * productDetails.discountPercentage/100).toFixed(2)}</b> $</span>  
+                   <span className="m-1" style={{color:'red'}}> {productDetails.discountPercentage}%</span>
+                   <span className="m-1" style={{textDecoration:'line-through'}}>{productDetails.price}</span>
                    
                
                 </span>
@@ -84,23 +88,31 @@ export default function ProductDetails() {
               ):(
                   <span>{productDetails.price}</span>
               )}
+
+              <span className="ms-5">
+                    {productDetails.stock !== 0 ? (
+                  <d className="badge text-bg-success">On stock</d>
+                ) : (
+                  <span className="badge text-bg-secondary">out of stock</span>
+                )}
+              </span>
               
             </div>
-            <div className="col-6">
-            {productDetails.stock !== 0 ? (
-            <span className="badge text-bg-success">On stock</span>
-          ) : (
-            <span className="badge text-bg-secondary">out of stock</span>
-          )}
+            <div className="col-6 m-2">
+            
             </div>
-          </div>
-          <div className='col-4 col-xs-12  m-2 d-flex justify-center-start  align-items-center'>
+            <div className='  m-2 d-flex justify-content-center '>
           <Button variant="danger"  onClick={() => dispatch(decrementQuantity(productDetails.id))}>-</Button>
           <span className='mx-2 '>{quantity}</span>
           <Button variant="success" onClick={() => dispatch(incrementQuantity(productDetails?.id))}>+</Button>
        
         </div>
-        <Button style={{width:'50%',fontSize:'0.5rem' , borderRadius:'30px',border:'2px solid black'} } className="btn  text-center  my-2" onClick={() => 
+          </div>
+          
+
+        <div className="d-flex justify-content-center pt-5">
+        <Button 
+         style={{width:'50%',fontSize:'0.9rem' , borderRadius:'30px', backgroundColor:'#3384b3'} } className="btn  text-center  my-2 " onClick={() => 
                   dispatch(addToCart({
                     id: productDetails.id,
                     title: productDetails.title,
@@ -109,9 +121,11 @@ export default function ProductDetails() {
                     stock: productDetails.stock,
                     description: productDetails.description,
                     
-                  }))}>Add To Cart
+                  }))}>
+                    Add To Cart
         </Button>
 
+        </div>
       </div>
       </div>
     </Container>
