e686f62f268006fed336ff190d4852d4512bf2e1
Muhammed Ali AbdelAleem <132581860+mohammedaliem@users.noreply.github.com>
2023-11-08 10:57:03 +0200

    Update Register.js
    add register form and validation to handle errors

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,3 +1,140 @@
+// import * as React from 'react';
+// import Avatar from '@mui/material/Avatar';
+// import Button from '@mui/material/Button';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import TextField from '@mui/material/TextField';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+// import Link from '@mui/material/Link';
+// import Grid from '@mui/material/Grid';
+// import Box from '@mui/material/Box';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+// import Typography from '@mui/material/Typography';
+// import Container from '@mui/material/Container';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// function Copyright(props) {
+//   return (
+//     <Typography variant="body2" color="text.secondary" align="center" {...props}>
+//       {'Copyright © '}
+//       <Link color="inherit" href="">
+//         DentiBask
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
+
+
+// const defaultTheme = createTheme();
+
+// export default function SignUp() {
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const data = new FormData(event.currentTarget);
+//     console.log({
+//       email: data.get('email'),
+//       password: data.get('password'),
+//     });
+//   };
+
+//   return (
+//     <ThemeProvider theme={defaultTheme}>
+//       <Container component="main" maxWidth="xs">
+//         <CssBaseline />
+//         <Box
+//           sx={{
+//             marginTop: 8,
+//             display: 'flex',
+//             flexDirection: 'column',
+//             alignItems: 'center',
+//           }}
+//         >
+//           <Avatar sx={{ m: 1, bgcolor: '#AAE0FF' }}>
+//             <LockOutlinedIcon />
+//           </Avatar>
+//           <Typography component="h1" variant="h5">
+//             Sign up
+//           </Typography>
+//           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+//             <Grid container spacing={2}>
+//               <Grid item xs={12} sm={6}>
+//                 <TextField
+//                   autoComplete="given-name"
+//                   name="firstName"
+//                   required
+//                   fullWidth
+//                   id="firstName"
+//                   label="First Name"
+//                   autoFocus
+//                 />
+//               </Grid>
+//               <Grid item xs={12} sm={6}>
+//                 <TextField
+//                   required
+//                   fullWidth
+//                   id="lastName"
+//                   label="Last Name"
+//                   name="lastName"
+//                   autoComplete="family-name"
+//                 />
+//               </Grid>
+//               <Grid item xs={12}>
+//                 <TextField
+//                   required
+//                   fullWidth
+//                   id="email"
+//                   label="Email Address"
+//                   name="email"
+//                   autoComplete="email"
+//                 />
+//               </Grid>
+//               <Grid item xs={12}>
+//                 <TextField
+//                   required
+//                   fullWidth
+//                   name="password"
+//                   label="Password"
+//                   type="password"
+//                   id="password"
+//                   autoComplete="new-password"
+//                 />
+//               </Grid>
+//               <Grid item xs={12}>
+//                 <TextField
+//                   required
+//                   fullWidth
+//                   name="confirm password"
+//                   label="Confirm Password"
+//                   type="password"
+//                   id="password"
+//                   autoComplete="new-password"
+//                 />
+//               </Grid>
+//               <Grid item xs={12}>
+//                 <FormControlLabel
+//                   control={<Checkbox value="allowExtraEmails" color="primary" />}
+//                   label="I want to receive inspiration, marketing promotions and updates via email."
+//                 />
+//               </Grid>
+//             </Grid>
+//             <Button
+//               type="submit"
+//               fullWidth
+//               variant="contained"
+//               sx={{ mt: 3, mb: 2 }}
+//             >
+//               Sign Up
+//             </Button>
+//             <Grid container justifyContent="flex-end">
+//               <Grid item>
+//                 <Link href="#" variant="body2">
+//                   Already have an account? Sign in
+//                 </Link>
+//               </Grid>
+//             </Grid>
+//           </Box>
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
@@ -10,33 +147,25 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
+import { useForm } from 'react-hook-form';
 import Container from '@mui/material/Container';
+import { Link as RouterLink } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="">
-        DentiBask
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
+import './SignUp.css';
 
 const defaultTheme = createTheme();
 
 export default function SignUp() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    // Handle form submission logic here
   };
 
   return (
@@ -57,8 +186,9 @@ export default function SignUp() {
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
+              {/* First Name Field */}
               <Grid item xs={12} sm={6}>
                 <TextField
                   autoComplete="given-name"
@@ -68,8 +198,17 @@ export default function SignUp() {
                   id="firstName"
                   label="First Name"
                   autoFocus
+                  {...register('firstName', {
+                    required: 'First Name is required',
+                    pattern: {
+                      value: /^[A-Za-z]+$/,
+                      message: 'Invalid First Name',
+                    },
+                  })}
                 />
+                {errors.firstName && <span>{errors.firstName.message}</span>}
               </Grid>
+              {/* Last Name Field */}
               <Grid item xs={12} sm={6}>
                 <TextField
                   required
@@ -78,8 +217,17 @@ export default function SignUp() {
                   label="Last Name"
                   name="lastName"
                   autoComplete="family-name"
+                  {...register('lastName', {
+                    required: 'Last Name is required',
+                    pattern: {
+                      value: /^[A-Za-z]+$/,
+                      message: 'Invalid Last Name',
+                    },
+                  })}
                 />
+                {errors.lastName && <span>{errors.lastName.message}</span>}
               </Grid>
+              {/* Email Field */}
               <Grid item xs={12}>
                 <TextField
                   required
@@ -88,8 +236,17 @@ export default function SignUp() {
                   label="Email Address"
                   name="email"
                   autoComplete="email"
+                  {...register('email', {
+                    required: 'Email is required',
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                      message: 'Invalid email address',
+                    },
+                  })}
                 />
+                {errors.email && <span>{errors.email.message}</span>}
               </Grid>
+              {/* Password Field */}
               <Grid item xs={12}>
                 <TextField
                   required
@@ -99,45 +256,79 @@ export default function SignUp() {
                   type="password"
                   id="password"
                   autoComplete="new-password"
+                  {...register('password', {
+                    required: 'Password is required',
+                    minLength: {
+                      value: 8,
+                      message: 'Password must be at least 8 characters long',
+                    },
+                  })}
                 />
+                {errors.password && <span>{errors.password.message}</span>}
               </Grid>
+              {/* Confirm Password Field */}
               <Grid item xs={12}>
                 <TextField
                   required
                   fullWidth
-                  name="confirm password"
+                  name="confirmPassword"
                   label="Confirm Password"
                   type="password"
-                  id="password"
-                  autoComplete="new-password"
+                  id="confirmPassword"
+                  {...register('confirmPassword', {
+                    validate: (value) => value === getValues('password') || 'Passwords do not match',
+                  })}
                 />
+                {errors.confirmPassword && <span>{errors.confirmPassword.message}</span>}
               </Grid>
+              {/* Phone Number Field */}
               <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
+                <TextField
+                  required
+                  fullWidth
+                  name="phoneNumber"
+                  label="Phone Number"
+                  id="phoneNumber"
+                  {...register('phoneNumber', {
+                    pattern: {
+                      value: /^(01)[0-9]{9}$/,
+                      message: 'Invalid phone number',
+                    },
+                  })}
                 />
+                {errors.phoneNumber && <span>{errors.phoneNumber.message}</span>}
               </Grid>
             </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={<Checkbox value="allowExtraEmails" color="primary" />}
+                    label="I want to receive inspiration, marketing promotions and updates via email."
+                  />
+              </Grid>
+            {/* Submit Button */}
+            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
               Sign Up
             </Button>
-            <Grid container justifyContent="flex-end">
+            <Grid container justifyContent="center" alignItems="center" sx={{ mt: 2 }}>
               <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
+              {/* <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link> */}
+                  <RouterLink to="/login" variant="body2">
+                    Already have an account? Sign in
+                  </RouterLink>
               </Grid>
-            </Grid>
+          </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 5 }} />
       </Container>
     </ThemeProvider>
   );
 }
+
+//         </Box>
+//         <Copyright sx={{ mt: 5 }} />
+//       </Container>
+//     </ThemeProvider>
+//   );
+// }
