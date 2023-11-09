523ac32e33f0f469827c1e840382e6fc06cda836
mohammedali9810 <mohammedali9810@gmail.com>
2023-11-09 09:58:38 +0200

    Added the Typography Lib import.

--- src/components/dashboard/ordersdashboard/orders.jsx
+++ src/components/dashboard/ordersdashboard/orders.jsx
@@ -5,6 +5,8 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import CardActionArea from '@mui/material/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
 
 import {
   Table,
