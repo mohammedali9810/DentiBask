d46b8e04a7f3637039de61625d6da5b3cd9313da
mohammedali9810 <mohammedali9810@gmail.com>
2023-11-17 21:45:47 +0200

    Edits & Bug Fixes + Styling to the Paypal & Payment Options.

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,2 +1,2 @@
-export const CLIENT_ID = process.env.CLIENT_ID || "Ac8yGPftM80i4KRTjtLxJOQ1kUthWa6wgd1WoYaKLJAO0DWA5Ffzyr7n50RGPAHI7XidP14pz4UT-2ZI"
-export const APP_SECRET = process.env.APP_SECRET || "EJAqAgtFAjckY9xJ7FklZAn6rJ5mgPGcehNGsDAx0Q12g1EHPWSdJuuU4PiL5hlbmjPDWAyt9NSbfyrQ"
+export const CLIENT_ID = process.env.CLIENT_ID || "AZF9w_KR-tojwTTbu8EAnwSQrOgV9MdhQRWgO0QY7cg2Yseq_LxwV1XteHAh8APMCGdRM15itce7WfMc"
+export const APP_SECRET = process.env.APP_SECRET || "EO4_85Ps815_M-Dy3KRqyWedZFLELFA3oJo5Xv-ht_bThfjCiSTpSYJsbinbCN1lfPNwfE_b1LtQCDUQ"
