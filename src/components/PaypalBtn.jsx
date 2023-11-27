745a7ba08eee48fdadb1f31d315d6581eadaaca0
shaher2017 <shaherhero8@gmail.com>
2023-11-27 22:48:47 +0200

    fix orders for admin

---
+++ src/components/PaypalBtn.jsx
@@ -0,0 +1,135 @@
+import React, { useRef, useEffect } from "react";
+import axiosinstance from "../axiosconfig";
+import { useDispatch } from "react-redux";
+import { resetItem } from "../store/slices/cartslice";
+
+export default function Paypal({ onSuccess, onError, onCancel, cart }) {
+  const dispatch = useDispatch();
+  const paypal = useRef();
+  const orderid = useRef();
+
+  const resetlist = () => {
+    dispatch(resetItem());
+  };
+
+  useEffect(() => {
+    if (!window.paypal || !cart) {
+      console.error("PayPal script not loaded or cart is undefined.");
+      return;
+    }
+
+    const createorder = async () => {
+      const orderitem_set = await Promise.all(
+        cart.map(async (item) => ({
+          product_id: item.id,
+          quantity: item.quantity,
+        }))
+      );
+      console.log(orderitem_set);
+      try {
+        const csrfToken = await axiosinstance.get("/Products/get_csrf_token/");
+        const response = await axiosinstance.post(
+          "/User/create_order/",
+          { "orderitem_set": orderitem_set },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "X-CSRFToken": csrfToken.data.csrfToken,
+              Authorization:
+                "Bearer " + localStorage.getItem("dentibask-access-token"),
+            },
+            withCredentials: true,
+          }
+        );
+        console.log("the order id is:" + response.data['order_id']);
+        orderid.current = response.data['order_id'];
+      } catch (error) {
+        console.error("Error creating order:", error);
+      }
+    };
+
+    const approvedorder = async () => {
+      try {
+        console.log("sent trans");
+        const csrfToken = await axiosinstance.get("/Products/get_csrf_token/");
+        console.log(csrfToken);
+        console.log("the order id is:" + orderid.current);
+        const response = await axiosinstance.post(
+          "/User/add_transaction/",
+          { 'order_id': orderid.current },
+          {
+            headers: {
+              "Content-Type": 'multipart/form-data',
+              "X-CSRFToken": csrfToken.data.csrfToken,
+              Authorization:
+                "Bearer " + localStorage.getItem("dentibask-access-token"),
+            },
+            withCredentials: true,
+          }
+        );
+
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error creating order:", error);
+      }
+    };
+
+    const actions = window.paypal.Buttons({
+      style: {
+        layout: "vertical",
+        color: "blue",
+        shape: "pill",
+        label: "checkout",
+        height: 40,
+      },
+      createOrder: (data, actions, err) => {
+        const orderData = {
+          intent: "CAPTURE",
+          purchase_units: [
+            {
+              description: cart
+                .map((item) => {
+                  console.log(item.quantity);
+                  return item.title;
+                })
+                .join(", "),
+              amount: {
+                currency_code: "USD",
+                value: cart
+                  .reduce(
+                    (total, item) => total + item.price * item.quantity,
+                    0
+                  )
+                  .toFixed(2),
+              },
+            },
+          ],
+        };
+        createorder();
+        return actions.order.create(orderData);
+      },
+
+      onApprove: async (data, actions) => {
+        const order = await actions.order.capture();
+        approvedorder();
+        resetlist();
+        onSuccess(order);
+      },
+      onError: (err) => {
+        console.log(err);
+        onError(err);
+      },
+      onCancel: () => {
+        console.log("Payment canceled by user");
+      },
+    });
+    actions.render(paypal.current);
+    return () => actions.close();
+  }, [onSuccess, onError, onCancel, cart]);
+
+  return (
+    <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", alignItems:"center"}}>
+      <div style={{width:"50%"}} ref={paypal}></div>
+    </div>
+  );
+}
--- src/components/dashboard/ordersdashboard/orders.jsx
+++ src/components/dashboard/ordersdashboard/orders.jsx
@@ -1,14 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Pagination from '@mui/material/Pagination';
 import axiosinstance from '../../../axiosconfig';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CardActionArea from '@mui/material/CardActionArea'; 
-import Typography from '@mui/material/Typography';
 import { Select, MenuItem } from '@mui/material';
-
-
 import {
   Table,
   TableBody,
@@ -18,22 +11,13 @@ import {
   TableRow,
   Paper,
   Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
 } from '@mui/material';
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
+  const [originalOrders, setOriginalOrders] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage] = useState(10);
-  const [openSeeOrderDialog, setOpenSeeOrderDialog] = useState(false);
-  const [selectedOrder, setSelectedOrder] = useState(null);
-  const [orderItems, setOrderItems] = useState([]);
-  const [pages, setPages] = useState(1);
-  const [maxpages, setMaxPages] = useState(1);
+  const [maxPages, setMaxPages] = useState(1);
 
   useEffect(() => {
     fetchOrders();
@@ -43,123 +27,133 @@ const Orders = () => {
     axiosinstance
       .get(`/User/get_all_orders/?page=${currentPage}`)
       .then((res) => {
-          setOrders(res.data.orders);
-          setMaxPages(Math.ceil((res.data.count)/12));
+        setOrders(res.data.orders);
+        setOriginalOrders(res.data.orders);
+        setMaxPages(Math.ceil(res.data.count / 12));
       })
       .catch((err) => {
         console.error('Error fetching orders:', err);
       });
   };
 
-  const fetchOrderItems = (orderId) => {
-    axiosinstance
-      .get(`/User/get_items_in_order/${orderId}`)
-      .then((res) => {
-        setOrderItems(res.data.items);
-      })
-      .catch((err) => {
-        console.error('Error fetching order items:', err);
-      });
-  };
-
-
   const handlePageChange = (event, page) => {
     setCurrentPage(page);
   };
 
   const handleSeeOrder = (order) => {
-    setSelectedOrder(order);
-    fetchOrderItems(order.id); 
-    setOpenSeeOrderDialog(true);
+    // Handle displaying details of the selected order (if needed)
   };
-
-
-  const handleCloseSeeOrderDialog = () => {
-    setOpenSeeOrderDialog(false);
-  };
-
 
   const handleStatusChange = (event, orderId) => {
     const newStatus = event.target.value;
-  
+
+    // Update the status in the local state
     setOrders((prevOrders) =>
       prevOrders.map((order) =>
         order.id === orderId ? { ...order, status: newStatus } : order
       )
     );
-  
-    axiosinstance
-  .post(`/User/change_order_status/${orderId}/`, { new_status: newStatus })
-  .then((res) => {
-    console.log('Order status update response:', res.data.order);
-  })
-  .catch((err) => {
-    console.error('Error updating order status:', err);
-    // Handle the error gracefully, e.g., show an error message to the user
-  });
   };
-  
-  const handleSaveOrderStatus = (orderId) => {
-    // Make an API call to save the updated status in the database
-    axiosinstance
-      .post(`/User/save_order_status/${orderId}/`)
-      .then((res) => {
-        console.log('Order status saved successfully:', res.data.order);
-      })
-      .catch((err) => {
-        console.error('Error saving order status:', err);
-        // Handle the error gracefully, e.g., show an error message to the user
-      });
-  };
-  
 
+  const isStatusModified = (orderId) => {
+    // Check if the status is modified by comparing with the original status
+    const originalOrder = originalOrders.find((order) => order.id === orderId);
+    const currentOrder = orders.find((order) => order.id === orderId);
+
+    return (
+      originalOrder &&
+      currentOrder &&
+      originalOrder.status !== currentOrder.status
+    );
+  };
+
+  const handleSaveOrderStatus = async (orderId) => {
+    try {
+        const csrfToken = await axiosinstance.get("/Products/get_csrf_token/");
+        console.log(csrfToken.data.csrfToken);
+
+        await axiosinstance.patch(
+            `/User/change_order_status/${orderId}/`,
+            {
+                new_status: orders.find((order) => order.id === orderId).status,
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrfToken.data.csrfToken,
+                    'Authorization': 'Bearer ' + localStorage.getItem('dentibask-access-token'),
+                },
+            }
+        );
+        console.log("done successfully");
+    } catch (error) {
+        console.error(error);
+    }
+};
   return (
     <div>
       <TableContainer component={Paper}>
         <Table>
-          <TableHead style={{backgroundColor:"#2196f3", width:"100%"}}>
+          <TableHead style={{ backgroundColor: '#2196f3', width: '100%' }}>
             <TableRow>
-              <TableCell style={{textAlign:"center", fontSize:"1.2rem"}}>Order ID</TableCell>
-              <TableCell style={{textAlign:"center", fontSize:"1.2rem"}}>Customer</TableCell>
-              <TableCell style={{textAlign:"center", fontSize:"1.2rem"}}>Date</TableCell>
-              <TableCell style={{textAlign:"center", fontSize:"1.2rem"}}>Total Price</TableCell>
-              <TableCell style={{textAlign:"center", fontSize:"1.2rem"}}>Status</TableCell>
-              <TableCell style={{textAlign:"center", fontSize:"1.2rem"}}>Action</TableCell>
+              <TableCell style={{ textAlign: 'center', fontSize: '1.2rem' }}>
+                Order ID
+              </TableCell>
+              <TableCell style={{ textAlign: 'center', fontSize: '1.2rem' }}>
+                Customer
+              </TableCell>
+              <TableCell style={{ textAlign: 'center', fontSize: '1.2rem' }}>
+                Date
+              </TableCell>
+              <TableCell style={{ textAlign: 'center', fontSize: '1.2rem' }}>
+                Total Price
+              </TableCell>
+              <TableCell style={{ textAlign: 'center', fontSize: '1.2rem' }}>
+                Status
+              </TableCell>
+              <TableCell style={{ textAlign: 'center', fontSize: '1.2rem' }}>
+                Action
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {orders.map((order) => (
               <TableRow key={order.id}>
-                <TableCell style={{textAlign:"center", fontSize:"1.2rem"}}>{order.id}</TableCell>
-                <TableCell style={{textAlign:"center", fontSize:"1.2rem"}}>{order.user}</TableCell>
-                <TableCell style={{textAlign:"center", fontSize:"1.2rem"}}>{order.created_at}</TableCell>
-                <TableCell style={{textAlign:"center", fontSize:"1.2rem"}}>${order.total}</TableCell>
-                <TableCell style={{textAlign:"center", fontSize:"1.2rem"}}>
-         <Select
-          value={order.status}
-          onChange={(e) => handleStatusChange(e, order.id)}
-        >
-
-    <MenuItem value="Canceled" style={{ color: 'red' }}>
-      Canceled
-      
-    </MenuItem>
-    <MenuItem value="Processing" style={{ color: 'blue' }}>
-      Processing
-    </MenuItem>
-    <MenuItem value="Shipped" style={{ color: 'gray' }}>
-      Shipped
-    </MenuItem>
-    <MenuItem value="Delivered" style={{ color: 'green' }}>
-      Delivered
-    </MenuItem>
-    <MenuItem value="Other" style={{ color: 'black' }}>
-      Other
-    </MenuItem>
-  </Select>
-</TableCell>
-
-                <TableCell style={{textAlign:"center", fontSize:"1.2rem"}}>
+                <TableCell style={{ textAlign: 'center', fontSize: '1.2rem' }}>
+                  {order.id}
+                </TableCell>
+                <TableCell style={{ textAlign: 'center', fontSize: '1.2rem' }}>
+                  {order.user}
+                </TableCell>
+                <TableCell style={{ textAlign: 'center', fontSize: '1.2rem' }}>
+                  {order.created_at}
+                </TableCell>
+                <TableCell style={{ textAlign: 'center', fontSize: '1.2rem' }}>
+                  ${order.total}
+                </TableCell>
+                <TableCell style={{ textAlign: 'center', fontSize: '1.2rem' }}>
+                  <Select
+                    value={order.status}
+                    onChange={(e) => handleStatusChange(e, order.id)}
+                  >
+                    <MenuItem value="Canceled" style={{ color: 'red' }}>
+                      Canceled
+                    </MenuItem>
+                    <MenuItem value="Processing" style={{ color: 'blue' }}>
+                      Processing
+                    </MenuItem>
+                    <MenuItem value="Shipped" style={{ color: 'gray' }}>
+                      Shipped
+                    </MenuItem>
+                    <MenuItem value="Delivered" style={{ color: 'green' }}>
+                      Delivered
+                    </MenuItem>
+                    <MenuItem value="Other" style={{ color: 'black' }}>
+                      Other
+                    </MenuItem>
+                  </Select>
+                </TableCell>
+                <TableCell style={{ textAlign: 'center', fontSize: '1.2rem' }}>
                   <Button
                     onClick={() => handleSeeOrder(order)}
                     variant="outlined"
@@ -168,13 +162,15 @@ const Orders = () => {
                     See Order
                   </Button>
 
-                  <Button
-          onClick={() => handleSaveOrderStatus(order.id)}
-          variant="outlined"
-          color="primary"
-        >
-          Save
-        </Button>
+                  {isStatusModified(order.id) && (
+                    <Button
+                      onClick={() => handleSaveOrderStatus(order.id)}
+                      variant="outlined"
+                      color="primary"
+                    >
+                      Save
+                    </Button>
+                  )}
                 </TableCell>
               </TableRow>
             ))}
@@ -186,88 +182,10 @@ const Orders = () => {
         <Pagination
           page={currentPage}
           onChange={handlePageChange}
-          count={maxpages}
+          count={maxPages}
           color="primary"
         />
       </div>
-
-      <Dialog
-        open={openSeeOrderDialog}
-        onClose={handleCloseSeeOrderDialog}
-        style={{ minWidth: 700 }}
-
-        >   
-        
-             <DialogTitle>Order Detail</DialogTitle>
-        <DialogContent>
-          <DialogContentText
-                  style={{ minWidth: 500 }}
-
-          >
-            {selectedOrder && (
-              <>
-                <div>
-                  <strong>Order Id:</strong> {selectedOrder.orderId}
-                </div>
-                <div>
-                  <strong>Customer Name:</strong> {selectedOrder.customerName}
-                </div>
-                <div>
-                  <strong>Created At:</strong> {selectedOrder.created_at}
-                </div>
-                <div>
-                  <strong>Total Price:</strong> ${selectedOrder.total}
-                </div>
-                <div>
-                  <strong>Number Of Products:</strong> {selectedOrder.numberOfProduct}
-                </div>
-                <div>
-                  <strong>Status:</strong> {selectedOrder.status}
-                </div>
-
-                <Typography variant="h6" gutterBottom>
-                  Products
-                </Typography>
-
-                {orderItems.map((product) => (
-                  <Card key={product.id}>
-                    <CardActionArea>
-                      <CardMedia
-                        component="img"
-                        height="200"
-                        image={product.thumbnail}
-                        alt={product.name}
-                      />
-                      <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                          {product.name}
-                        </Typography>
-                        <span style={{ fontSize: '1rem', fontWeight: 'bold' }}>
-                          {product.description}
-                        </span>
-                        <p style={{ fontWeight: 'bold', fontSize: '1rem' }}>
-                          Price: ${product.price}
-                        </p>
-                        <p style={{ fontWeight: 'bold', fontSize: '1rem' }}>
-                          Category: {product.category}
-                        </p>
-                        <p style={{ fontWeight: 'bold', fontSize: '1rem' }}>
-                          Ordered: {product.ordered} times
-                        </p>
-                      </CardContent>
-                    </CardActionArea>
-                  </Card>
-                ))}
-              </>
-            )}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseSeeOrderDialog} color="primary">
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
     </div>
   );
 };
